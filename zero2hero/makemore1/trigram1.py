a2f046558c2b19ef1a401eccc054eead52db2cac
Emir Habul <emir@tillion.io>
2023-03-30 00:22:16 +0800

    second video [3/n] more simple models

--- zero2hero/makemore1/trigram1.py
+++ zero2hero/makemore1/trigram1.py
@@ -4,13 +4,13 @@ import random
 words = open('names.txt', 'r').read().splitlines()
 f = defaultdict(str)
 for word in words:
-    word = "^" + word + "$"
-    for a, b in zip(word, word[1:]):
-        f[a] += b
+    word = "^^" + word + "$"
+    for a, b, c in zip(word, word[1:], word[2:]):
+        f[a + b] += c
 
 for _ in range(100):
-    current_word = '^'
+    current_word = '^^'
     while current_word[-1] != '$':
-        current_word += random.choice(f[current_word[-1:]])
-    current_word = current_word[1:-1]
+        current_word += random.choice(f[current_word[-2:]])
+    current_word = current_word[2:-1]
     print(current_word, '***' if current_word in words else '')
