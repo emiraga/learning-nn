1523bec2b4698eac1d2d892c888cf7186e3df355
Emir Habul <emir@tillion.io>
2023-04-03 12:03:16 +0800

    second video [4/n] generating N

--- zero2hero/makemore1/bigram2.py
+++ zero2hero/makemore1/bigram2.py
@@ -1,10 +1,16 @@
 from collections import defaultdict
 import random
+import string
 import torch
 
+allChars = '^$' + string.ascii_lowercase
+stoi = {ch: i for i, ch in enumerate(allChars)}
+itos = {i: ch for i, ch in enumerate(allChars)}
+N = torch.zeros((28, 28), dtype=torch.int32)
 words = open('names.txt', 'r').read().splitlines()
 for word in words:
-    pass
+    chs = ['^'] + list(word) + ['$']
+    for ch1, ch2 in zip(chs, chs[1:]):
+        N[stoi[ch1], stoi[ch2]] += 1
 
-N = torch.zeros((28, 28), dtype=torch.int32)
 print(N)
