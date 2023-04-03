8a0357701688b13d6571ce619a425536fc021d3c
Emir Habul <emir@tillion.io>
2023-04-03 14:03:18 +0800

    second video [6/n] bigram using tensors

--- zero2hero/makemore1/bigram2.py
+++ zero2hero/makemore1/bigram2.py
@@ -4,27 +4,35 @@ import random
 import string
 import torch
 
-allChars = '.' + string.ascii_lowercase + ','
+allChars = '.' + string.ascii_lowercase
 s2i = {ch: i for i, ch in enumerate(allChars)}
 i2s = {i: ch for i, ch in enumerate(allChars)}
 N = torch.zeros((28, 28), dtype=torch.int32)
 words = open('names.txt', 'r').read().splitlines()
 for word in words:
-    chs = ['.'] + list(word) + [',']
+    chs = ['.'] + list(word) + ['.']
     for ch1, ch2 in zip(chs, chs[1:]):
         N[s2i[ch1], s2i[ch2]] += 1
 
-print(N)
-
-# plt.imshow(N)
+# plt.figure(figsize=(10,10))
+# plt.imshow(N, cmap='Blues')
+# for i in range(len(allChars)):
+#     for j in range(len(allChars)):
+#         chstr = i2s[i] + i2s[j]
+#         plt.text(j, i, chstr, ha="center", va="bottom", color='gray', fontdict={"size": 5})
+#         plt.text(j, i, str(N[i, j].item()), ha="center", va="top", color='gray', fontdict={"size": 6})
+# plt.axis('off')
 # plt.show()
 
-plt.figure(figsize=(10,10))
-plt.imshow(N, cmap='Blues')
-for i in range(len(allChars)):
-    for j in range(len(allChars)):
-        chstr = i2s[i] + i2s[j]
-        plt.text(j, i, chstr, ha="center", va="bottom", color='gray', fontdict={"size": 5})
-        plt.text(j, i, str(N[i, j].item()), ha="center", va="top", color='gray', fontdict={"size": 6})
-plt.axis('off')
-plt.show()
+
+g = torch.Generator().manual_seed(2147483647)
+for i in range(5):
+  out = []
+  ix = 0
+  while True:
+    p = N[ix].float()
+    ix = int(torch.multinomial(p, num_samples=1, replacement=True, generator=g).item())
+    out.append(i2s[ix])
+    if ix == 0:
+      break
+  print(''.join(out))
